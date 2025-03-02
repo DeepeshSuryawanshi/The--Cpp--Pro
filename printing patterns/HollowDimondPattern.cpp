b771120bc101174b21dc84fb849d94f13cccfcca
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2025-03-02 16:48:24 +0530

    Pattern nexted loops are done

--- printing patterns/HollowDimondPattern.cpp
+++ printing patterns/HollowDimondPattern.cpp
@@ -9,20 +9,40 @@ int main(){
         {// spaces
             cout<<" ";
         }
+        
         //star print 
         cout<<"*";
         
        //star print 
        if (i != 0)
        {
-        for (int k = 0; k < 2*i-1; k++)
-        {
-           cout<<" ";
-        }
-        cout<<"*";
+            for (int k = 0; k < 2*i-1; k++)
+            {
+                cout<<" ";
+            }
+            cout<<"*";
        }
-       
         cout<<endl;
     }
+    //bottom
+    for (int i = 0; i < n-1; i++)
+    {   
+        for (int j = 0; j < i+1; j++)
+        {// spaces
+            cout<<" ";
+        }
+
+        cout<<"*";
+        if(i != n-2 ){
+            for (int j = 0; j < 2*(n-i)-5; j++)
+            {
+                cout<<" ";
+            }
+            
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
     
 }
