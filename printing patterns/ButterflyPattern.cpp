c983b630e96ffc641449f937d061ae39046bf44a
Deepesh suryawanshi <deepeshsuryawanshi04@gmail.com>
2025-03-30 14:54:15 +0530

    complete butter fly pattern problem

--- printing patterns/ButterflyPattern.cpp
+++ printing patterns/ButterflyPattern.cpp
@@ -4,12 +4,48 @@ using namespace std;
 int main(){
     int n = 4 ;
     // top left
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= i+1; j++)
         {
             cout<<"*";
-        }
+        };
+        for (int j = n-1; j > i; j--)
+        {
+            cout<<" ";
+        };
+        for (int j = n-1; j > i; j--)
+        {
+            cout<<" ";
+        };
+        for (int j = 1; j <= i+1; j++)
+        {
+            cout<<"*";
+        };
         cout<<endl;
     }
+    // bottom
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > i+1; j--)
+        {
+            cout<<"*";
+        };
+       for (int j = 0; j < i+1; j++)
+       {
+        cout<<" ";
+       };
+       for (int j = 0; j < i+1; j++)
+       {
+        cout<<" ";
+       };
+       for (int j = n; j > i+1; j--)
+        {
+            cout<<"*";
+        };
+       
+       
+        cout<<endl;
+    }
+    
 }
